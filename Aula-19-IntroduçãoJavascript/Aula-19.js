95abb81f4afd8e7a49f8b16833c39d22321750f6
Maressa Castro <maressa.pco651@gmail.com>
2023-10-30 21:33:20 -0300

    adição java

--- Aula-19-IntroduçãoJavascript/Aula-19.js
+++ Aula-19-IntroduçãoJavascript/Aula-19.js
@@ -5,4 +5,26 @@ let fruta = {
     tipo: "fruta"
 }
 
-console.log(fruta);
+let fruta2 = {
+    nome: "banana",
+    cor: "amarela",
+    preco: 4.59,
+    tipo: "fruta"
+}
+let fruta3 = {
+    nome: "goiaba",
+    cor: "verde",
+    preco: 4.59,
+    tipo: "fruta"
+}
+let fruta4 = {
+    nome: "pessego",
+    cor: "rosa",
+    preco: 4.59,
+    tipo: "fruta"
+}
+
+let frutas =[fruta, fruta2, fruta3, fruta4]
+
+
+console.log(frutas[fruta4]);
---
+++ Aula-21-JavaScript na Web II/javaAula21.js
@@ -0,0 +1,7 @@
+let nome = document.createElement("h1");
+nome.innerText = "Maressa";
+
+document.body.append(nome);
+
+let sobrenome = document.getElementById("h2");
+sobrenome.remove(;)

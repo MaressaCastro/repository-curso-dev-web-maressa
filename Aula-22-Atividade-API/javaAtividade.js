604fd565e3404e245f4838e5d89048c0dee41fd7
Maressa Castro <maressa.pco651@gmail.com>
2023-11-05 08:48:14 -0300

    criando atividade java

---
+++ Aula-22-Atividade-API/javaAtividade.js
@@ -0,0 +1,22 @@
+function buscarBandeira() {
+  
+    let paisDigitado = document.getElementById("paisDigitado")
+
+    let url = 'https://restcountries.com/v3.1/name/' + paisDigitado.value;
+
+    fetch(url)
+        .then(response => response.json())
+        .then(response => exibirPais(response))
+}
+
+function exibirPais(pais) {
+ 
+    let divImg = document.getElementById("bandeira")
+
+    divImg.innerText = ""
+
+    let bandeira = document.createElement("img")
+    bandeira.src = pais[0].flags.png
+
+    divImg.append(bandeira)
+}
